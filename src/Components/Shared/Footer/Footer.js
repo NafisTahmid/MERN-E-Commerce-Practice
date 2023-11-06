02407ece05e440cd54770141d8090024be16f0d0
NafisTahmid <nafistahmid4@gmail.com>
2023-11-06 11:11:40 +0600

    First Commit

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -3,7 +3,8 @@ import React from 'react';
 const footer = () => {
     return (
         <section className="pb-3">
-            <p className='text-center'><a className="text-muted text-decoration-none" href="https://nafistahmid.github.io/my_portfolio/" target="_blank" rel="noreferrer">Nafis Tahmid</a> | {(new Date()).getFullYear()}</p>
+            {/* <p className='text-center'><a className="text-muted text-decoration-none" href="https://nafistahmid.github.io/my_portfolio/" target="_blank" rel="noreferrer">MERN E-Commerce</a> | {(new Date()).getFullYear()}</p> */}
+            <p className='text-center text-muted text-decoration-none'>MERN E-Commerce | {(new Date()).getFullYear()}</p>
         </section>
     );
 };
